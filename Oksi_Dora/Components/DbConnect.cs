245db591c2bf0002e28a4e6876d9b51c1c5f7680
232013 <232013@WINMAC2>
2022-10-27 09:44:06 +0300

    Добавьте файлы проекта.

---
+++ Oksi_Dora/Components/DbConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oksi_Dora.Components
+{
+    internal class DbConnect
+    {
+        public static OksFord_DoraEntities db = new OksFord_DoraEntities();
+    }
+}
---
+++ Oksi_Dora/Pages/AuthPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Oksi_Dora.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oksi_Dora.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Auth1BTN_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.NextPage(new Nav("Регистрация", new RegistrPage()));
+        }
+
+        private void LoginBTN_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTb.Text.Trim();
+            string password = PasswordTb.Password.Trim();
+
+            var userA = DbConnect.db.User.ToList().Find(x => x.Login == login && x.Password == password);
+            if (userA == null)
+            {
+                MessageBox.Show("Такого пользователя нет", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                Navigation.isAuth = true;
+                Navigation.NextPage(new Nav("Список услуг", new ServiceListPage()));
+            }
+        }
+    }
+}
